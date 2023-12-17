65f997f6b2b7a5217fc4bb5b3ee7754b7336ab8d
yumin2231 <138893349+yumin2231@users.noreply.github.com>
2023-12-18 00:51:32 +0900

    yuminQuakeliveOfficalSite

--- app/WebApp/EqMap/Ver2/script.js
+++ app/WebApp/EqMap/Ver2/script.js
@@ -3,7 +3,7 @@ var PolygonLayer_Style_nerv = {
     "color": "#ffffff",
     "weight": 1.5,
     "opacity": 1,
-    "fillColor": "#3a3a3a",
+    "fillColor": "#404040",
     "fillOpacity": 1
 }
 $.getJSON("prefectures.geojson", function (data) {
