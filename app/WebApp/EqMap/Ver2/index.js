2a5ca535e771452d597cdbc0d27bec38bc2703dd
yumin2231 <138893349+yumin2231@users.noreply.github.com>
2023-12-17 20:26:58 +0900

    yuminQuakeliveOfficalSite

--- app/WebApp/EqMap/Ver2/index.js
+++ app/WebApp/EqMap/Ver2/index.js
@@ -1,17 +1,13 @@
-var map = L.map('map',{
-    zoomControl: false
-})
-var map = L.map('map').setView([37.7102, 139.8132], 6);
-L.control.scale({ maxWidth: 150, position: 'bottomright', imperial: false }).addTo(map);
-map.zoomControl.setPosition('topright');
+var map = L.map('map',{zoomControl: false}).setView([37.7102, 139.8132], 6);
 var PolygonLayer_Style_nerv = {
-"color": "#ffffff",
-"weight": 1.5,
-"opacity": 1,
-"fillColor": "#3a3a3a",
-"fillOpacity": 1}
+    "color": "#ffffff",
+    "weight": 1.5,
+    "opacity": 1,
+    "fillColor": "#3a3a3a",
+    "fillOpacity": 1
+}
 $.getJSON("prefectures.geojson", function (data) {
-L.geoJson(data, {
-style: PolygonLayer_Style_nerv
-}).addTo(map);
-}); 
+    L.geoJson(data, {
+        style: PolygonLayer_Style_nerv
+    }).addTo(map);
+});
