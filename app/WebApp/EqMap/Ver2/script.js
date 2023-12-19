20298e12474b89ea04e23881f661d006fd5b8b4b
yumin2231 <138893349+yumin2231@users.noreply.github.com>
2023-12-20 01:01:23 +0900

    yuminQuakeliveOfficalSite

--- app/WebApp/EqMap/Ver2/script.js
+++ app/WebApp/EqMap/Ver2/script.js
@@ -11,3 +11,15 @@ $.getJSON("prefectures.geojson", function (data) {
         style: PolygonLayer_Style_nerv
     }).addTo(map);
 });
+$.getJSON("https://api.p2pquake.net/v2/history?codes=551", function (data) {
+    console.log(data["0"]["earthquake"]["hypocenter"]["name"]);
+    var [time, name, shindo, magnitude, depth] = [
+        data["0"]["earthquake"]["time"],
+        data["0"]["earthquake"]["hypocenter"]["name"],
+        data["0"]["earthquake"]["maxScale"],
+        data["0"]["earthquake"]["hypocenter"]["magnitude"],
+        data["0"]["earthquake"]["hypocenter"]["depth"]
+    ]
+    console.log(time+"ごろ、"+name+"で最大震度"+shindo/10+"の地震が発生しました。マグニチュードは"+magnitude+"、深さ"+depth+"kmと推定されています。");
+    
+});
