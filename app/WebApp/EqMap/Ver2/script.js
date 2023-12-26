765f37d138a74dd063047f11f9c6cd4baa1c93b9
yumin2231 <138893349+yumin2231@users.noreply.github.com>
2023-12-27 03:57:52 +0900

    yuminQuakeliveOfficalSite

--- app/WebApp/EqMap/Ver2/script.js
+++ app/WebApp/EqMap/Ver2/script.js
@@ -30,6 +30,29 @@ $.getJSON("https://api.p2pquake.net/v2/history?codes=551", function (data) {
         popupAnchor: [0,-40]
         });
         var shingenIcon = L.marker(shingenLatLng, {icon: shingenIconImage }).addTo(map);
+
+        let maxInt_data = data[0]['earthquake']['maxScale'];
+    var maxIntText = maxInt_data == 10 ? "1" : maxInt_data == 20 ? "2" : maxInt_data == 30 ? "3" : maxInt_data == 40 ? "4" :
+                     maxInt_data == 45 ? "5弱" : maxInt_data == 46 ? "5弱" : maxInt_data == 50 ? "5強" : maxInt_data == 55 ? "6弱" :
+                     maxInt_data == 60 ? "6強" : maxInt_data == 70 ? "7" : "不明";
+    
+    var Magnitude = data[0]['earthquake']['hypocenter']['magnitude'] != -1 ?
+                    (data[0]['earthquake']['hypocenter']['magnitude']).toFixed(1) : 'ー.ー';
+    var Name = data[0]['earthquake']['hypocenter']['name'] != "" ?
+               data[0]['earthquake']['hypocenter']['name'] : '情報なし';
+    var Depth = data[0]['earthquake']['hypocenter']['depth'] != -1 ?
+                "約"+data[0]['earthquake']['hypocenter']['depth']+"km" : '不明';
+    var tsunamiText = data[0]['earthquake']['domesticTsunami'] == "None" ? "なし" :
+                      data[0]['earthquake']['domesticTsunami'] == "Unknown" ? "不明" :
+                      data[0]['earthquake']['domesticTsunami'] == "Checking" ? "調査中" :
+                      data[0]['earthquake']['domesticTsunami'] == "NonEffective" ? "若干の海面変動" :
+                      data[0]['earthquake']['domesticTsunami'] == "Watch" ? "津波注意報" :
+                      data[0]['earthquake']['domesticTsunami'] == "Warning" ? "津波警報" : "情報なし";
+    var Time = data[0]['earthquake']['time'];
+    shingenIcon.bindPopup('発生時刻：'+Time+'<br>最大震度：'+maxIntText+'<br>震源地：'+Name+'<span style=\"font-size: 85%;\"> ('+data[0]["earthquake"]["hypocenter"]["latitude"]+", "+data[0]["earthquake"]["hypocenter"]["longitude"]+')</span><br>規模：M'+Magnitude+'　深さ：'+Depth+'<br>受信：'+data[0]['issue']['time']+', '+data[0]['issue']['source'],{closeButton: false, zIndexOffset: 10000, maxWidth: 10000});
+    shingenIcon.on('mouseover', function (e) {this.openPopup();});
+    shingenIcon.on('mouseout', function (e) {this.closePopup();});
+
 });
 function requestFullscreen(elem) {
     // 全画面表示をリクエストするメソッドを取得
